5dc2ba7e7728644eef047a0275067c2efb01479b
Raphael Napinas <rcnapinas@cpp.edu>
2021-09-24 18:57:46 -0700

    Got game to work, need to implement max number change

--- numberGuess.c
+++ numberGuess.c
@@ -22,9 +22,40 @@ int main()
             // change max number
         }
 
-        printf("Let's Play the Guessing Game!\n");
-        target = rand() % maxNumber + 1;
-        printf("A random number has been chosen!\nThis number is between 1 and %d\n", maxNumber);
+        printf("Let's Play the Guessing Game!\n\n");
+
+        target = (rand() % maxNumber) + 1;
+        printf("A random number has been chosen!\nThis number is between 1 and %d\n\n", maxNumber);
+
+        while (guess != target)
+        {
+            printf("Guess a number between 1 and %d: ", maxNumber);
+            scanf("%d", &guess);
+
+            if (guess == target)
+            {
+                printf("Correct! You guessed the number!\n\n");
+            }
+            else if (guess < target)
+            {
+                printf("Your guess was smaller than target! Try again!\n\n");
+            }
+            else
+            {
+                printf("Your guess was bigger than the target! Try again!\n\n");
+            }
+        }
+
+        printf("Do you want to play again?\n");
+        printf("Press 1 to play a game\nPress 2 to change the max number\nPress 3 to quit\n");
+        scanf("%d", &keepPlaying);
+
+        while(keepPlaying < 1 && keepPlaying > 3)
+        {
+            printf("Please pick a valid option.\n");
+            printf("Press 1 to play a game\nPress 2 to change the max number\nPress 3 to quit\n");
+            scanf("%d", &keepPlaying);
+        }
     }
 
     printf("Thanks for playing!");
